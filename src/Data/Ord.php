8b493750a5e10835d017c5bfe83614aa59f0d590
0x1 <kevin.francart3@gmail.com>
2026-07-03 23:29:52 +0200

    fix: Ord partial app

--- src/Data/Ord.php
+++ src/Data/Ord.php
@@ -1,6 +1,12 @@
 <?php
 
-$Data_Ord_ordIntImpl = function ($lt, $eq, $gt, $x, $y) {
+$Data_Ord_ordIntImpl = function ($lt, $eq = null, $gt = null, $x = null, $y = null) {
+    if (func_num_args() === 3) return function ($x, $y = null) use ($lt, $eq, $gt) {
+        if (func_num_args() === 1) return function ($y) use ($lt, $eq, $gt, $x) {
+            return $x < $y ? $lt : ($x === $y ? $eq : $gt);
+        };
+        return $x < $y ? $lt : ($x === $y ? $eq : $gt);
+    };
     return $x < $y ? $lt : ($x === $y ? $eq : $gt);
 };
 
