9f675a78f573acbbcfcc073f9540db7e33b3120a
0x1 <kevin.francart3@gmail.com>
2026-07-07 19:33:39 +0200

    Update

--- src/Data/Ord.php
+++ src/Data/Ord.php
@@ -20,4 +20,35 @@ $exports['ordStringImpl'] = $ordStringImpl;
 $exports['ordNumberImpl'] = $ordNumberImpl;
 $exports['ordCharImpl'] = $ordCharImpl;
 $exports['ordBooleanImpl'] = $ordBooleanImpl;
+
+$ordArrayImpl = function($f, $xs = null, $ys = null) use (&$ordArrayImpl) {
+    if (func_num_args() < 3) {
+        $__args = func_get_args();
+        return function(...$more) use ($__args, &$ordArrayImpl) {
+            return $ordArrayImpl(...array_merge($__args, $more));
+        };
+    }
+    
+    $i = 0;
+    $xlen = count($xs);
+    $ylen = count($ys);
+    while ($i < $xlen && $i < $ylen) {
+        $x = $xs[$i];
+        $y = $ys[$i];
+        $o = $f($x, $y);
+        if ($o !== 0) {
+            return $o;
+        }
+        $i++;
+    }
+    if ($xlen === $ylen) {
+        return 0;
+    } else if ($xlen > $ylen) {
+        return -1;
+    } else {
+        return 1;
+    }
+};
+
+$exports['ordArrayImpl'] = $ordArrayImpl;
 return $exports;
