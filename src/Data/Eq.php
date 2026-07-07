89e9374ffd8fc3b1cb06b7411ef74a55f86c0f3a
0x1 <kevin.francart3@gmail.com>
2026-07-07 17:16:55 +0200

    Add eqArrayImpl FFI

--- src/Data/Eq.php
+++ src/Data/Eq.php
@@ -15,9 +15,24 @@ $eqNumberImpl = $eqIntImpl;
 $eqCharImpl = $eqIntImpl;
 $eqBooleanImpl = $eqIntImpl;
 
+$eqArrayImpl = function($f, $xs = null, $ys = null) use (&$eqArrayImpl) {
+    if (func_num_args() < 3) {
+        $__args = func_get_args();
+        return function(...$more) use ($__args, &$eqArrayImpl) {
+            return $eqArrayImpl(...array_merge($__args, $more));
+        };
+    }
+    if (count($xs) !== count($ys)) return false;
+    for ($i = 0; $i < count($xs); $i++) {
+        if (!$f($xs[$i], $ys[$i])) return false;
+    }
+    return true;
+};
+
 $exports['eqIntImpl'] = $eqIntImpl;
 $exports['eqStringImpl'] = $eqStringImpl;
 $exports['eqNumberImpl'] = $eqNumberImpl;
 $exports['eqCharImpl'] = $eqCharImpl;
 $exports['eqBooleanImpl'] = $eqBooleanImpl;
+$exports['eqArrayImpl'] = $eqArrayImpl;
 return $exports;
